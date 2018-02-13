52c9c8f753e5badfc4486fe46a9dcfb5aeb1ae98
cameron fuller <cameronmfuller@yahoo.com>
2018-02-12 22:56:47 -0500

    static app

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-// import characters from "./characters.json;"
+import characters from "./characters.json";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Navpills from "./components/Navpills";
-// import Card from "./components/Card";
+import Card from "./components/Card";
 
 
 class App extends Component {
+
+  state = {
+    characters
+  };
+
   render() {
     return (
       <Wrapper>
         <Navpills />
         <Title />
+        {this.state.characters.map(character => (
+          <Card
+            name={character.name}
+            image={character.image}
+          />
+        ))}
       </Wrapper>
     );
   }
